ceb5b6ec8335882a7047a4c91d1564a1f08b0a8f
Blair Brownlie <bbrownli@asu.edu>
2019-04-02 23:38:49 -0700

    setting up the final pieces of my bootstrap stylings

---
+++ assets/javascript/firebase.js
@@ -0,0 +1,13 @@
+$(function(){
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDRpk7ADMH_QbIhzpgUqpJF4h7EYeGW3vI",
+    authDomain: "train-scheduler-13682.firebaseapp.com",
+    databaseURL: "https://train-scheduler-13682.firebaseio.com",
+    projectId: "train-scheduler-13682",
+    storageBucket: "",
+    messagingSenderId: "673055671894"
+  };
+  firebase.initializeApp(config);
+  var database = firebase.database();
+})
